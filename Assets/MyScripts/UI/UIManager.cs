3ee096733fc8956481b959b99f1930214a7df22c
Alin <alin.lupu@info.uaic.ro>
2015-04-21 20:39:01 +0300

    ui

--- Assets/MyScripts/UI/UIManager.cs
+++ Assets/MyScripts/UI/UIManager.cs
@@ -4,12 +4,28 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour
 {
-    public HUDComponent onScreenCanvas;
+    private HUDComponent onScreenCanvasHUDComponent;
+
     // Use this for initialization
     void Start()
     {
+      //  try
+       // {
+         //   GameObject canvas = GameObject.FindGameObjectWithTag("OnScreenCanvas");
+         //   onScreenCanvasHUDComponent = canvas.GetComponent<HUDComponent>();
+         //   printOnScreenMessage("Welcome! Вяш эи пытынтёюм ылаборарэт, эи видэ лебыр дылыктуч эжт. Хабэо факилиз позтюлант ед мэя, дольор кончэтытюр ан нык. Дуо трётанё трактатоз нэ. Векж дёко едквюэ ыт, про ан алиё эрат констятюам, конгуы лыгэндоч патриоквюы ед эож.", 25);
+    
+   //     }
+     //   catch(UnityException)
+       // {
+            InitializeHUDComponent();
+            GameObject canvas = GameObject.FindGameObjectWithTag("OnScreenCanvas");
+            onScreenCanvasHUDComponent = canvas.GetComponent<HUDComponent>();
+            printOnScreenMessage("Welcome! Вяш эи пытынтёюм ылаборарэт, эи видэ лебыр дылыктуч эжт. Хабэо факилиз позтюлант ед мэя, дольор кончэтытюр ан нык. Дуо трётанё трактатоз нэ. Векж дёко едквюэ ыт, про ан алиё эрат констятюам, конгуы лыгэндоч патриоквюы ед эож.", 25);
+    
+        //}
         
-    }
+     }
 
     // Update is called once per frame
     void Update()
@@ -17,11 +33,53 @@ public class UIManager : MonoBehaviour
 
     }
     
+    public void InitializeHUDComponent()
+    {
+        OVRPlayerController player = GameObject.FindObjectOfType<OVRPlayerController>();
+        
+            GameObject hudGO;
+            GameObject hudTextGO;
+            GameObject hudPanelBkgrGO;
+
+            hudGO = new GameObject();
+            hudTextGO = new GameObject();
+            hudPanelBkgrGO = new GameObject();
+
+            hudGO.name = "hudCanvasGO";
+            hudGO.tag = "OnScreenCanvas";
+            hudTextGO.name = "hudTextGO";
+            hudPanelBkgrGO.name = "hudPanelBkgrGO";
+
+            hudGO.transform.parent = player.transform;
+            hudTextGO.transform.parent = hudPanelBkgrGO.transform;
+            hudPanelBkgrGO.transform.parent = hudGO.transform;
+            
+            hudGO.AddComponent<Canvas>();
+            hudGO.AddComponent<RectTransform>();
+            hudGO.AddComponent<CanvasScaler>();
+            hudGO.AddComponent<GraphicRaycaster>();
+            hudGO.AddComponent<HUDComponent>();
+            
+            hudPanelBkgrGO.AddComponent<RectTransform>();
+            hudPanelBkgrGO.AddComponent<CanvasRenderer>();
+            hudPanelBkgrGO.AddComponent<Image>();
+
+            hudTextGO.AddComponent<RectTransform>();
+            hudTextGO.AddComponent<CanvasRenderer>();
+            hudTextGO.AddComponent<Text>();
+            
+            hudGO.GetComponent<HUDComponent>().InitiateSettings();
+    }
+
+
     public void printOnScreenMessage(string text, float timeToShow)
     {
-        onScreenCanvas.showCanvas();
-        onScreenCanvas.setText(text);
-        onScreenCanvas.setTimer(timeToShow);
+        if (onScreenCanvasHUDComponent != null)
+        {
+            onScreenCanvasHUDComponent.ShowCanvas();
+            onScreenCanvasHUDComponent.SetText(text);
+            onScreenCanvasHUDComponent.SetTimer(timeToShow);
+        }
     }
 
 }
