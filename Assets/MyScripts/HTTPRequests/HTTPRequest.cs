26abbfe50440de732fc2b28cf5ae6389badac04a
Alin <alin.lupu@info.uaic.ro>
2015-05-28 21:49:45 +0300

    voilla

---
+++ Assets/MyScripts/HTTPRequests/HTTPRequest.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HTTPRequest : MonoBehaviour {
+
+    void Start() { }
+
+    public WWW GET(string url)
+    {
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForRequest(www));
+        return www;
+    }
+
+    public WWW POST(string url, Dictionary<string, string> post)
+    {
+        WWWForm form = new WWWForm();
+        foreach (KeyValuePair<string, string> post_arg in post)
+        {
+            form.AddField(post_arg.Key, post_arg.Value);
+        }
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(WaitForRequest(www));
+        return www;
+    }
+
+    public WWW POST(string url, string postString)
+    {
+        WWWForm form = new WWWForm();
+        Dictionary<string, string> headers = new Dictionary<string, string>();
+        byte[] rawData = form.data;
+
+        headers.Add("Content-Type", "application/json;charset=UTF-8");
+        //headers.Add("Content-Encoding", "text/plain");
+        rawData = System.Text.Encoding.UTF8.GetBytes(postString.ToCharArray());
+       
+
+        WWW www = new WWW(url, rawData, headers);
+      
+
+
+        StartCoroutine(WaitForRequest(www));
+        return www;
+    }
+
+    private IEnumerator WaitForRequest(WWW www)
+    {
+        while(!www.isDone){}
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+   
+}
---
+++ Assets/MyScripts/HTTPRequests/RequestManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RequestManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void sendGameData(string jsonToSend)
+    {
+        HTTPRequest request = new HTTPRequest();
+        request.POST("http://172.17.254.180:8080/api/oculusUsersWithEvents", jsonToSend);
+    }
+
+    public void sendEvent(string jsonToSend)
+    {
+        HTTPRequest request = new HTTPRequest();
+        request.POST("http://172.17.254.180:8080/api/oculusEventsWithUsers", jsonToSend);
+    }
+}
