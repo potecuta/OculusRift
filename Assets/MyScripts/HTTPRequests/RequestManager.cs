23c1ec688baf8324dc38ed9946322ce524a9eba8
Alin <alin.lupu@info.uaic.ro>
2015-06-01 01:58:13 +0300

    sa nu mai fie null...

--- Assets/MyScripts/HTTPRequests/RequestManager.cs
+++ Assets/MyScripts/HTTPRequests/RequestManager.cs
@@ -1,28 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Threading;
 
 public class RequestManager : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
+     void Start()
+    {
+    
+    }
+
+
+    
 
 	// Update is called once per frame
 	void Update () {
-	
+        
 	}
+
 
     public void sendGameData(string jsonToSend)
     {
-        HTTPRequest request = new HTTPRequest();
+        HTTPRequest request = (HTTPRequest)GameObject.FindGameObjectWithTag("GlobalManager").GetComponent("HttpRequest");
         request.POST("http://172.17.254.180:8080/api/oculusUsersWithEvents", jsonToSend);
     }
 
     public void sendEvent(string jsonToSend)
     {
-        HTTPRequest request = new HTTPRequest();
+        HTTPRequest request = (HTTPRequest)GameObject.FindGameObjectWithTag("GlobalManager").GetComponent("HttpRequest");
         request.POST("http://172.17.254.180:8080/api/oculusEventsWithUsers", jsonToSend);
     }
 }
