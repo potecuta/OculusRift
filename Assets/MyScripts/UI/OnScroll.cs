e1db2b6bf4b6b367b0728b196310ab9132e1b6a4
Alin <alin.lupu@info.uaic.ro>
2015-06-11 13:32:35 +0300

    ui

---
+++ Assets/MyScripts/UI/OnScroll.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OnScroll : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+        if (togglel == null)
+        {
+            togglel = GameObject.Find("Form");
+
+            initPosition = togglel.transform.localPosition;
+        }
+	}
+
+    Vector3 initPosition;
+    GameObject togglel;
+
+    public void onScroll()
+    {
+
+        Scrollbar scrollBar = GameObject.FindGameObjectWithTag("Scroll").GetComponent<Scrollbar>();
+      
+      GameObject.Find ("Form").transform.localPosition = initPosition + new Vector3(0, scrollBar.value*7000f*Time.smoothDeltaTime,0);
+    
+    }
+}
